b0aa3765377f0d8fd843cef6681b3b9a4a4fca1b
Zinger <kylinar88@gmail.com>
2020-05-03 23:35:25 +0300

    HomeScreen : add popup for create remind event

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Text, ImageBackground, StyleSheet } from 'react-native';
-import { Button, Container, Header, Content, List, ListItem } from 'native-base';
+import { ImageBackground, StyleSheet, Modal, TouchableHighlight, View } from 'react-native';
+import { Button, Container, Header, Content, List, ListItem, Text, Textarea } from 'native-base';
 import * as firebase from 'firebase';
 
 const reminders = [
@@ -28,7 +28,9 @@ export class HomeScreen extends React.Component {
     state = {
         email: '',
         displayName: '',
-        reminders: reminders
+        modalVisible: false,
+        reminders: reminders,
+        textCache: ''
     }
 
     componentDidMount() {
@@ -44,17 +46,19 @@ export class HomeScreen extends React.Component {
         console.log('tut');
     }
 
-    addRemind = () => {
+    addRemind = (title) => {
         const newRemind = {
             id: Math.random() * 21,
-            title: 'Заметка',
+            title: title ? title : 'Заметка без названия',
             date: Date.now(),
             description: 'новая заметка'
         }
-
+        
         this.setState({
-            reminders: [...this.state.reminders, newRemind]
-        })
+            reminders: [...this.state.reminders, newRemind],
+            textCache: '',
+            modalVisible: false
+        });
     }
 
     removeRemind = (id) => () => {
@@ -65,16 +69,66 @@ export class HomeScreen extends React.Component {
         })
     }
 
+    writeRemind = (text) => {
+        this.setState({
+            textCache: text
+        })
+    }
+
+    setModalVisible = (visible) => {
+        this.setState({ 
+            modalVisible: visible,
+            textCache: '' 
+        });
+    }
 
     render() {
         return(
             <ImageBackground source={{uri: 'https://i.pinimg.com/originals/4c/7a/b1/4c7ab1da89e96e9051005526164af8ed.jpg'}} style={{width: '100%', height: '100%', opacity: 0.7}}>
                 <Container style={style.container}>
+                    <Modal
+                        animationType="slide" 
+                        transparent={true}
+                        visible={this.state.modalVisible}
+                        onRequestClose={() => {
+                            Alert.alert("Modal has been closed.");
+                        }}
+                    >
+                        <View
+                            style={style.modalContainer}
+                        >
+                            <View
+                                style={style.modalWhiteWindow}
+                            >
+                                <TouchableHighlight
+                                    style={style.modalCloseButton}
+                                    onPress={() => {
+                                        this.setModalVisible(!this.state.modalVisible);
+                                    }}
+                                >
+                                    <Text>x</Text>
+                                </TouchableHighlight>
+                                <View style={style.modalView}>
+                                    <Text style={{marginBottom: 10}}>Write your remind</Text>
+                                    <Textarea
+                                        onChangeText={text => this.writeRemind(text)}
+                                        style={style.modalTextarea}
+                                    ></Textarea>
+                                    <Button
+                                        onPress={()=> this.addRemind(this.state.textCache)}
+                                    >
+                                        <Text>Готово!</Text>
+                                    </Button>
+                                </View>
+                            </View>
+                        </View>
+                    </Modal>
+
                     <Content>
                         <Text style={style.title}>{`Yo ${this.state.email}, lets check you business`}</Text>
                         <Button 
                             //onPress={this.createRemind}
-                            onPress={this.addRemind}
+                            onPress={() => this.setModalVisible(!this.state.modalVisible)}
                             light 
                             block 
                             style={{paddingHorizontal: 15}}>
@@ -125,5 +179,39 @@ const style = new StyleSheet.create({
         borderBottomColor: '#333',
         borderBottomWidth: 1,
         marginBottom: 20
+    },
+    modalContainer: {
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0,0,0,0.8)'
+    },
+    modalView: {
+        
+    }, 
+    modalWhiteWindow: {
+        position: 'relative',
+        backgroundColor: 'white',
+        padding: 20,
+        width: '90%',
+        borderRadius: 5
+    }, 
+    modalTextarea: {
+        borderWidth: 1, 
+        borderColor: '#eee',
+        borderRadius: 3,
+        marginBottom: 10
+    }, 
+    modalCloseButton: {
+        position: 'absolute',
+        width: 20, 
+        height: 20, 
+        borderRadius: 20,
+        borderWidth: 1, 
+        borderColor: '#e7e7e7',
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: 10,
+        right: 10,
     }
 })
